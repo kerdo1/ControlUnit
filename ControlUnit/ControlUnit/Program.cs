c116599757be9294c5b7f935baef7aaad99ce757
kerdo1 <kerdovahk@gmail.com>
2024-08-27 09:21:57 +0300

    init

--- ControlUnit/ControlUnit/Program.cs
+++ ControlUnit/ControlUnit/Program.cs
@@ -28,7 +28,7 @@ namespace ControlUnit
 
 			app.MapControllerRoute(
 				name: "default",
-				pattern: "{controller=Home}/{action=Privacy}/{id?}");
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
 			app.Run();
 		}
