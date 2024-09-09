a7d3426929d40d8b638ed84b1293f7e7187c7b9a
kerdo1 <kerdovahk@gmail.com>
2024-09-09 11:45:22 +0300

    create view added

--- ControlUnit/ControlUnit/Controllers/StudentsController.cs
+++ ControlUnit/ControlUnit/Controllers/StudentsController.cs
@@ -1,38 +1,42 @@
 ﻿using ControlUnit.Data;
+using ControlUnit.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ControlUnit.Controllers
 {
-	public class StudentsController : Controller
-	{
-		private readonly SchoolContext _context;
-        public StudentsController(SchoolContext context)
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController
+            (
+            SchoolContext context
+            )
         {
-			_context = context;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
-		{
-			var result = await _context.Students.ToListAsync();
+        {
+            var result = await _context
+                .Students.ToListAsync();
 
+            return View(result);
+        }
 
-			return View(result);
-		}
-
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
             var student = await _context.Students
                 .Include(s => s.Enrollments)
-                .ThenInclude(e => e.Course)
+                    .ThenInclude(e => e.Course)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.Id == id);
-
 
             if (student == null)
             {
@@ -40,6 +44,32 @@ namespace ControlUnit.Controllers
             }
 
             return View(student);
-		}
-	}
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes" + "try again");
+            }
+            return View(student);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
 }
